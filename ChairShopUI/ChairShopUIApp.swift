76d59497e11321b8638bd9743b4431d3e24b9485
Xing <zhaoxing8325@gmail.com>
2023-03-20 12:44:43 -0700

    Initial Commit

---
+++ ChairShopUI/ChairShopUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChairShopUIApp.swift
+//  ChairShopUI
+//
+//  Created by XING ZHAO on 2023-03-20.
+//
+
+import SwiftUI
+
+@main
+struct ChairShopUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
